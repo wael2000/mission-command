aa2737bedc2e9d67355074d990b50d290f1e6593
wael2000 <weldoami@redhat.com>
2024-05-09 11:20:08 +0200

    Adding more Gitops assets

---
+++ gitops/clear.sh
@@ -0,0 +1,6 @@
+# SNO
+oc login --token=sha256~7eP-uFTkaiBIpOO4V6fQLYp211hg-iRNvcxnD8YI2W8 --server=https://api.cluster-rg8x2.dynamic.redhatworkshops.io:6443 --insecure-skip-tls-verify=true
+oc delete project mc-azure
+# Oher
+oc login --token=sha256~7v_njfbBrH2lkjBuHF3LArQKwucY0oYkfrXRsNanxKU --server=https://api.cluster-f44mq.dynamic.redhatworkshops.io:6443 --insecure-skip-tls-verify=true
+oc delete project mc-datacenter
