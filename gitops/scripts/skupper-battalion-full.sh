077af363cdf55565320533a81b7c5071cfa545e1
wael2000 <weldoami@redhat.com>
2024-05-28 10:19:33 +0200

    Done

---
+++ gitops/scripts/skupper-battalion-full.sh
@@ -0,0 +1,137 @@
+DC_URL=https://api.cluster-gg776.gg776.sandbox2839.opentlc.com:6443
+DC_UID=admin
+DC_PWD=P@ssw0rd
+
+AWS_URL=https://api.cluster-vk4dx.vk4dx.sandbox1545.opentlc.com:6443
+AWS_UID=admin
+AWS_PWD=OTAwMTc1
+
+# DC
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+
+skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name dc-site
+skupper token create aws_to_dc.token 
+
+
+#AWS
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+
+skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name aws-site
+skupper link create aws_to_dc.token --name aws-to-dc
+
+
+# DC
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service create postgresql 5432 --protocol tcp
+skupper service bind postgresql service postgresqldb
+
+#AWS
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service bind postgresql service postgresqldb
+
+# every API component will access the DB next to it (least expensive route)
+# UI->dc-dc
+# UI->aws-aws
+
+# DC
+# unbind the service in DC to see the impact
+skupper service unbind postgresql service postgresqldb
+
+# API components in both sites will connect to DB on aws
+# UI->dc-aws
+# UI->aws-aws
+
+
+# =======================================
+
+
+oc project battalion-hauk
+skupper init --enable-console --enable-flow-collector --console-auth unsecured
+skupper token create aws_to_azure.token
+
+oc project battalion-hauk-azure
+skupper init --enable-console --enable-flow-collector --console-auth unsecured
+skupper link create aws_to_azure.token --name aws-to-azure
+skupper token create azure_to_aws.token
+oc project battalion-hauk
+skupper link create azure_to_aws.token --name azure-to-aws
+skupper network status
+
+
+# expose 
+skupper expose service postgresqldb --address postgresql --port 5432 -n battalion-hauk
+skupper service delete postgresql -n battalion-hauk
+
+skupper expose service azure-postgresql-service.battalion-hauk-azure.svc.cluster.local --address postgresql --port 5432 -n battalion-hauk-azure
+skupper service delete postgresql -n battalion-hauk-azure
+
+# create and bind
+skupper service create postgresql 5432 --protocol tcp -n battalion-hauk
+
+
+skupper service bind postgresql service postgresqldb -n battalion-hauk
+
+skupper service bind postgresql service postgresqldb.battalion-hauk.svc.cluster.local  -n battalion-hauk
+skupper service bind postgresql service azure-postgresql-service.battalion-hauk-azure.svc.cluster.local  -n battalion-hauk-azure
+
+
+
+skupper service unbind postgresql service postgresqldb.battalion-hauk.svc.cluster.local  -n battalion-hauk
+
+
+# delete 
+oc project battalion-hauk
+skupper delete 
+oc project battalion-hauk-azure
+skupper delete 
+
+
+
+
+#Azure-Central India(ARO Cluster) 
+#Expose the service
+skupper expose service backend.backend-ns --port 8080 --protocol tcp --address backend
+skupper expose service backend.backend-ns --port 8080 --protocol tcp --address backend
+
+
+
+
+
+
+
+
+
+#skupper unbind service  postgresql service azure-postgresql-service -n battalion-hauk-azure
+
+skupper service create postgresql 5432 --protocol tcp -n battalion-hauk
+skupper service create postgresql 5432 --protocol tcp -n battalion-hauk-azure
+
+skupper service bind postgresql service postgresqldb -n battalion-hauk
+skupper service bind postgresql service azure-postgresql-service -n battalion-hauk-azure
+
+
+
+#skupper service create postgresql 5432 --protocol tcp
+#skupper service bind postgresql service postgresqldb
+#oc project battalion-hauk-azure
+#skupper service bind postgresql service azure-postgresql-service
+#skupper service unbind postgresql service postgresqldb
+
+
+#skupper expose service postgresqldb --address postgresql --port 5432
+#skupper expose service postgresqldb --address postgresql --port 5432
+
+#skupper expose service azure-postgresql-service --address postgresql --port 5432 --target-namespace battalion-hauk-azure
+
+
+
+#oc project battalion-hauk
+#skupper service delete postgresql
+#skupper service unbind postgresql service postgresqldb
+
+#oc project battalion-hauk-azure
+#skupper unexpose service azure-postgresql-service --address postgresql
--- gitops/scripts/skupper-battalion.sh
+++ gitops/scripts/skupper-battalion.sh
@@ -1,117 +1,175 @@
-# DC
-skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name dc-site
-skupper token create aws_to_dc.token  
+DC_URL=https://api.cluster-gg776.gg776.sandbox2839.opentlc.com:6443
+DC_UID=admin
+DC_PWD=P@ssw0rd
 
+AWS_URL=https://api.cluster-vk4dx.vk4dx.sandbox1545.opentlc.com:6443
+AWS_UID=admin
+AWS_PWD=OTAwMTc1
+
+AZURE_URL=https://api.azure-vk4dx-1.vk4dx-1.sandbox1507.opentlc.com:6443
+AZURE_UID=admin
+AZURE_PWD=OTAwMTc1
+
+# A hello world bash function
+menu () {
+  # https://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Connect
+clear
+echo "       _\|/_"
+echo "       (o o)"
+echo " +..oOO.{-}.OOo............................. select an option ....+"
+echo " |                                                                |"
+echo " | ██████╗ ██████╗ ███╗   ██╗███╗   ██╗███████╗ ██████╗████████╗  |"
+echo " | ██╔════╝██╔═══██╗████╗  ██║████╗  ██║██╔════╝██╔════╝╚══██╔══╝ |"
+echo " | ██║     ██║   ██║██╔██╗ ██║██╔██╗ ██║█████╗  ██║        ██║    |"
+echo " | ██║     ██║   ██║██║╚██╗██║██║╚██╗██║██╔══╝  ██║        ██║    |"
+echo " | ╚██████╗╚██████╔╝██║ ╚████║██║ ╚████║███████╗╚██████╗   ██║    |"
+echo " | ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═╝     |"
+echo " +................................................................+.................................+"
+echo " | DC                       | AWS                     | Azure                 | Azure Native        |"
+echo " +..................................................................................................+"
+echo " | 1) init DC               | 2) init and link        | 3) init and link      | 4) init and link    |"
+echo " | 5) expose db service     | 8) bind service         | 10) bind service      | 12) bind service    |"
+echo " | 6) bind service          | 9) unbind service       | 11) unbind service    | 13) unbind service  |"
+echo " | 7) unbind service        |                         |                       |                     |" 
+echo " | 14) delete all           |                         |                       |                     |" 
+echo " +..................................................................................................+"
+
+if [ "$1" = "done" ]; then
+echo " step $2 ==> $1 "
+fi
+}
+
+menu 
+
+while true
+do 
+read n
+case $n in
+"1")
+# DC
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name dc-site
+skupper token create aws_to_dc.token 
+skupper token create azure_to_dc.token
+skupper token create azure_native_to_dc.token
+;;
+
+"2")
 #AWS
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
 skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name aws-site
 skupper link create aws_to_dc.token --name aws-to-dc
+;;
 
+"3")
+#AZURE
+oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name aws-site
+skupper link create aws_to_dc.token --name azure-to-dc
+;;
+
+"4")
+# Azure Native DB
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team-azure
+skupper init --enable-console --enable-flow-collector --console-auth unsecured --site-name azure-native-site
+skupper link create azure_native_to_dc.token --name azure-native-to-dc
+;;
+
+
+"5")
 # DC
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
 skupper service create postgresql 5432 --protocol tcp
+;;
+
+
+"6")
+# DC
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
 skupper service bind postgresql service postgresqldb
+;;
 
-#AWS
-skupper service bind postgresql service postgresqldb
 
-# every API component will access the DB next to it (least expensive route)
-# UI->dc-dc
-# UI->aws-aws
-
+"7")
 # DC
 # unbind the service in DC to see the impact
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
 skupper service unbind postgresql service postgresqldb
-
-# API components in both sites will connect to DB on aws
-# UI->dc-aws
-# UI->aws-aws
+;;
 
 
-# =======================================
+"8")
+# bind service on AWS
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service bind postgresql service postgresqldb
+;;
 
 
-oc project battalion-hauk
-skupper init --enable-console --enable-flow-collector --console-auth unsecured
-skupper token create aws_to_azure.token
-
-oc project battalion-hauk-azure
-skupper init --enable-console --enable-flow-collector --console-auth unsecured
-skupper link create aws_to_azure.token --name aws-to-azure
-skupper token create azure_to_aws.token
-oc project battalion-hauk
-skupper link create azure_to_aws.token --name azure-to-aws
-skupper network status
+"9")
+# unbind service on AWS
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service unbind postgresql service postgresqldb
+;;
 
 
-# expose 
-skupper expose service postgresqldb --address postgresql --port 5432 -n battalion-hauk
-skupper service delete postgresql -n battalion-hauk
-
-skupper expose service azure-postgresql-service.battalion-hauk-azure.svc.cluster.local --address postgresql --port 5432 -n battalion-hauk-azure
-skupper service delete postgresql -n battalion-hauk-azure
-
-# create and bind
-skupper service create postgresql 5432 --protocol tcp -n battalion-hauk
+"10")
+# bind service on Azure
+oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service bind postgresql service postgresqldb
+;;
 
 
-skupper service bind postgresql service postgresqldb -n battalion-hauk
-
-skupper service bind postgresql service postgresqldb.battalion-hauk.svc.cluster.local  -n battalion-hauk
-skupper service bind postgresql service azure-postgresql-service.battalion-hauk-azure.svc.cluster.local  -n battalion-hauk-azure
-
-
-
-skupper service unbind postgresql service postgresqldb.battalion-hauk.svc.cluster.local  -n battalion-hauk
+"11")
+# unbind service on Azure
+oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper service unbind postgresql service postgresqldb
+;;
 
 
-# delete 
-oc project battalion-hauk
-skupper delete 
-oc project battalion-hauk-azure
-skupper delete 
+"12")
+# bind Azure Native DB
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team-azure
+skupper service bind postgresql service azure-postgresql-service
+;;
 
-
-
-
-#Azure-Central India(ARO Cluster) 
-#Expose the service
-skupper expose service backend.backend-ns --port 8080 --protocol tcp --address backend
-skupper expose service backend.backend-ns --port 8080 --protocol tcp --address backend
+"13")
+# unbind Azure Native DB
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team-azure
+skupper service unbind postgresql service azure-postgresql-service
+;;
 
 
 
+"14")
+oc login --server=$DC_URL -u $DC_UID -p $DC_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper delete
+oc project battalion-fox-team-azure
+skupper delete
+oc login --server=$AWS_URL -u $AWS_UID -p $AWS_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper delete
+oc login --server=$AZURE_URL -u $AZURE_UID -p $AZURE_PWD --insecure-skip-tls-verify=true
+oc project battalion-fox-team
+skupper delete
+;;
 
 
+esac
 
+#menu "done" $n
 
-
-
-#skupper unbind service  postgresql service azure-postgresql-service -n battalion-hauk-azure
-
-skupper service create postgresql 5432 --protocol tcp -n battalion-hauk
-skupper service create postgresql 5432 --protocol tcp -n battalion-hauk-azure
-
-skupper service bind postgresql service postgresqldb -n battalion-hauk
-skupper service bind postgresql service azure-postgresql-service -n battalion-hauk-azure
-
-
-
-#skupper service create postgresql 5432 --protocol tcp
-#skupper service bind postgresql service postgresqldb
-#oc project battalion-hauk-azure
-#skupper service bind postgresql service azure-postgresql-service
-#skupper service unbind postgresql service postgresqldb
-
-
-#skupper expose service postgresqldb --address postgresql --port 5432
-#skupper expose service postgresqldb --address postgresql --port 5432
-
-#skupper expose service azure-postgresql-service --address postgresql --port 5432 --target-namespace battalion-hauk-azure
-
-
-
-#oc project battalion-hauk
-#skupper service delete postgresql
-#skupper service unbind postgresql service postgresqldb
-
-#oc project battalion-hauk-azure
-#skupper unexpose service azure-postgresql-service --address postgresql
+done
