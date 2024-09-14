2402b7afa424ec126fc8dd84630fb9dcd39c7da1
wael2000 <weldoami@redhat.com>
2024-09-14 09:23:19 +0200

    add frontend

--- jdk.sh
+++ jdk.sh
@@ -1,4 +1,9 @@
-/usr/libexec/java_home -V 
+/usr/libexec/java_home -V
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.8.jdk/Contents/Home
 export TESTCONTAINERS_RYUK_DISABLED=true
 # https://quarkus.io/blog/quarkus-devservices-testcontainers-podman/
+
+# JDK 17
+export JAVA_HOME=/usr/local/opt/openjdk@17
+
+#export PATH="/usr/local/opt/openjdk@17/bin:$PATH
